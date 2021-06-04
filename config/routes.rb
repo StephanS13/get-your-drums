53eefcab36aada7cf7dac50ef35ce72f4882012c
Stephane Simcic <simcic.stephane@gmail.com>
2021-06-04 16:05:21 +0800

    Adds seeds, modifies routes pb

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,16 @@ Rails.application.routes.draw do
   get 'products/show'
   get 'accounts/show'
   get 'pages/home'
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # solves "No route matches [GET] "/users/sign_out""
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  resources :products, only: [:index, :show]
 
   root to: "pages#home"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "destroy all"
+
+# Order.destroy_all
+# User.destroy_all
+# Product.destroy_all
+
+puts "creates users"
+
+stephan = User.new(first_name: "Stephane", last_name: "Simcic", email:"stephan@test.com", password:"dadada")
+# file = File.open(Rails.root.join('db/seeds/images/users/steph.jpg'))
+# steph.avatar.attach(io: file, filename: 'steph.jpg', content_type: 'image/jpg')
+# steph.save!
+
+puts "users created"
+
+puts "creates products"
+
+j_dilla = Product.new(category: "drums",
+                      name: "J_Dilla",
+                      price: 19.90,
+                      content:  "20 kicks, 30 hats, 25 snares, 15 percussions, 6 drum breaks",
+                      description: "Known for his crisps productions, this drum kit gathers the best of Jay Dee’s drums style and basslines.
+                      Single hits or drum break loops, these sounds were sampled through the MPC 60 as well as processed through the SSL board to filter and give you the highest quality without compromising with Dilla’s reckonizable sound.",
+                      rating: 4,
+                      tags: "drums, drum kit, kick, snare, hat, J Dilla, J-Dilla, Dilla Dog",
+                      sound_name: "kick_1"
+                      )
+                      
+file = File.open(Rails.root.join('db/seeds/images/products/jd.jpg'))
+j_dilla.photo.attach(io: file, filename: 'jd.jpg', content_type: 'image/jpg')
+j_dilla.save!
+
+puts "products created"
