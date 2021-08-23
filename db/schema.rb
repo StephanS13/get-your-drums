745659b66c1ccba65ebbf041852cafcf43ff4fd2
Stephane Simcic <simcic.stephane@gmail.com>
2021-08-23 19:03:38 +0800

    changes Line_Items quantity to default: 1. Links product cards to cart.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_092516) do
+ActiveRecord::Schema.define(version: 2021_08_23_095941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_092516) do
   end
 
   create_table "line_items", force: :cascade do |t|
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.integer "product_id"
     t.integer "cart_id"
     t.integer "order_id"
